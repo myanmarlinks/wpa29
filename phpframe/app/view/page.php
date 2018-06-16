bc5fc28f63ba69514f901248024bde0b6d97c063
phpcrazy <phpcrazy@gmail.com>
2018-06-16 17:31:26 +0630

    PHP Frame

---
+++ phpframe/app/view/page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Page Area</title>
+</head>
+<body>
+	<h1>Page Area</h1>
+</body>
+</html>
--- phpframe/public/index.php
+++ phpframe/public/index.php
@@ -1,50 +1,12 @@
 <?php 
 
-$home = <<<HOME
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Home Page</title>
-</head>
-<body>
-	<h1>Home Page</h1>
-</body>
-</html>
-HOME;
-
-$blog = <<<BLOG
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Blog Area</title>
-	</head>
-	<body>
-		<h1>Blog Area</h1>
-	</body>
-	</html>
-BLOG;
-
-$page = <<<PAGE
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Page Area</title>
-</head>
-<body>
-	<h1>Page Area</h1>
-</body>
-</html>
-PAGE;
 
 if(!isset($_GET['page'])) {
-	echo $home;
+	include "../app/view/home.php";
 } elseif($_GET['page'] == "blog") {
-	echo $blog;
+	include "../app/view/blog.php";
 } elseif($_GET['page'] == 'page') {
-	echo $page;
+	include "../app/view/page.php";
 } else {
 	echo "404";
 }
