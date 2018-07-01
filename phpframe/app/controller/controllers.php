d82c994b754ab068b55556db3b9c21e5773e7676
phpcrazy <phpcrazy@gmail.com>
2018-07-01 18:11:36 +0630

    VueJS

--- phpframe/app/controller/controllers.php
+++ phpframe/app/controller/controllers.php
@@ -1,12 +1,14 @@
 <?php 
 
 function HomeController() {
+
+	$mystudents = '[{"id":"1","name":"Aung Aung","address":"Hledan"},{"id":"2","name":"Hla Hla","address":"Hledan"},{"id":"3","name":"Bo Bo Aung","address":"Hledan"},{"id":"4","name":"Myo Win","address":"Hledan"},{"id":"5","name":"Win Myo","address":"Hledan"},{"id":"6","name":"Sai Htee Sai","address":"Hledan"},{"id":"7","name":"Sai Sai","address":"Hledan"},{"id":"8","name":"Sai Sai Khan Hlaing","address":"Hledan"},{"id":"9","name":"G Fatt","address":"Hledan"},{"id":"10","name":"J Me","address":"Hledan"},{"id":"11","name":"Kyat Pha","address":"Hledan"},{"id":"12","name":"Wut Hmone Shwe Yee","address":"Hledan"},{"id":"13","name":"Goo Goo","address":"Pazuntaung"},{"id":"14","name":"Test Name","address":"Hlaing"}]';
+
 	$data = [
 		'home'		=> "Myanmar Links",
 		'another'	=> 'Test Another',
-		'students'	=> json_encode(_db_get("students"))
+		'students'	=> $mystudents
 	];
-
 
 	_load_view("home", $data);
 }
