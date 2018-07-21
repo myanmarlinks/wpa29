9f85157f9a8cad18a2a4cb4761e8a5c68f6800c7
phpcrazy <phpcrazy@gmail.com>
2018-07-21 18:51:37 +0630

    Vue Router

---
+++ vueroute/js/components/Blog.vue
@@ -0,0 +1,4 @@
+<template>
+	<h1>Hello from Blog</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut natus reprehenderit possimus, libero laboriosam minus ullam ipsam animi ex enim veniam. Deserunt nulla, quidem id architecto ad consequatur est quod.</p>
+</template>
---
+++ vueroute/js/components/Home.vue
@@ -0,0 +1,4 @@
+<template>
+	<h1>Hello From Home</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit quos iure temporibus facere, libero sunt aliquid adipisci officiis maxime. Voluptatem consequuntur odit aspernatur cumque ab dicta illo veniam ipsum dolore!</p>
+</template>
